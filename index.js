ca275c1e854f0cd97957fb1b65c078cc9e062bd1
Félix Scherer <felix.scherer@etu.umontpellier.fr>
2024-03-07 09:48:51 +0100

    docs: translating code, add comments & removing useless console logs

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*
+    Script inspired by @Pajul7's original creation.
+    @author felschrr
+*/
+
 // Import required modules
 const { Builder, By, until } = require("selenium-webdriver");
 const firefox = require("selenium-webdriver/firefox");
@@ -43,16 +48,13 @@ async function handleModalIfExists() {
     try {
         await driver.wait(
             until.elementLocated(By.className("app-modal")),
-            15000
+            5000
         );
         const modal = await driver.findElement(By.css("div.app-modal"));
         await driver.executeScript("arguments[0].remove();", modal);
         console.log("Pop-up modal found and removed.");
     } catch (error) {
-        console.log(
-            "Pop-up modal not found or error removing it:",
-            error.message
-        );
+        console.log("Pop-up modal not found or error removing it:");
     }
 }
 
@@ -73,14 +75,14 @@ async function handleModalIfExists() {
     try {
         const url = await prompt("Please enter a valid URL:\n");
         await driver.get(url);
-        console.log("Retrieving the Case on Hellcase.com...");
+        console.log("Loading the case from Hellcase.com...");
 
         // Check and handle modal after navigating to the URL
         await handleModalIfExists();
 
         // Initialize arrays to store data
-        let skins_global_probas = [];
-        let skin_Odds_Value_tuple = [];
+        let globalSkinProbabilities = [];
+        let skinOddsValueTuple = [];
 
         // Wait for page elements to load
         await driver.wait(
@@ -88,28 +90,26 @@ async function handleModalIfExists() {
             5000
         );
 
-        // Après avoir récupéré tous les éléments "item-wrap"
+        // After retrieving all the "item-wrap" elements
         let items = await driver.findElements(By.className("item-wrap"));
 
-        // Filtrer pour exclure les éléments qui ont un ancêtre "collapse accordion-item"
+        // Filter to exclude elements that have an ancestor "collapse accordion-item"
         items = await Promise.all(
             items.map(async (item) => {
                 let isDescendantOfAccordion = await driver.executeScript(
-                    `
-        return arguments[0].closest('.collapse.accordion-item') !== null;
-    `,
+                    `return arguments[0].closest('.collapse.accordion-item') !== null;`,
                     item
                 );
                 return isDescendantOfAccordion ? null : item;
             })
         );
 
-        // Supprimer les éléments null de la liste (ceux qui sont descendants d'un "collapse accordion-item")
+        // Remove null elements from the list (those that are descendants of a "collapse accordion-item")
         items = items.filter((item) => item !== null);
 
-        let case_length = items.length; // Cette ligne reste inchangée, mais case_length sera maintenant basé sur les items filtrés
+        let caseLength = items.length; // This line remains unchanged but caseLength is now based on filtered items
 
-        let current_skin = 0;
+        let currentSkin = 0;
         let item;
         // Retrieve the case price
         await driver.wait(
@@ -117,24 +117,22 @@ async function handleModalIfExists() {
             5000
         );
 
-        let case_price_text = await driver
+        let casePriceText = await driver
             .findElement(By.className("core-price--preset--default"))
             .getText();
 
-        let case_price = parseFloat(case_price_text.trim().substring(1));
+        let casePrice = parseFloat(casePriceText.trim().substring(1));
 
         // Loop to process each item
         for (let i = 0; i < items.length; i++) {
             // Retrieve global probability of the item
             console.log(
-                `Retrieving skins probabilities... ${parseInt(
-                    ((current_skin + 1) / case_length) * 100
-                )}%`
+                `Retrieving skin probabilities... (${i + 1}/${caseLength})`
             );
 
             item = items[i];
 
-            // Check if the item is not descendant of the div with class "accordion-item-header"
+            // Check if the item is not a descendant of the div with class "accordion-item-header"
             if (item) {
                 await driver.wait(
                     until.elementIsVisible(
@@ -143,15 +141,15 @@ async function handleModalIfExists() {
                     5000
                 );
 
-                let global_proba_text = await item
+                let globalProbabilityText = await item
                     .findElement(By.className("item-wrap__chance"))
                     .findElement(By.css("span:nth-child(2)"))
                     .getText();
 
-                let global_proba =
-                    parseFloat(global_proba_text.trim().slice(0, -1)) / 100;
+                let globalProbability =
+                    parseFloat(globalProbabilityText.trim().slice(0, -1)) / 100;
 
-                skins_global_probas.push(global_proba);
+                globalSkinProbabilities.push(globalProbability);
 
                 // Click on the item button to see details
                 await driver.wait(
@@ -176,21 +174,21 @@ async function handleModalIfExists() {
                     until.elementLocated(By.className("base-price__value")),
                     5000
                 );
-                let skinsValuesElements = await oddsTable.findElements(
+                let skinValuesElements = await oddsTable.findElements(
                     By.className("base-price__value")
                 );
 
-                let skinsValues = [];
-                for (let j = 0; j < skinsValuesElements.length; j++) {
-                    let valueElement = skinsValuesElements[j];
+                let skinValues = [];
+                for (let j = 0; j < skinValuesElements.length; j++) {
+                    let valueElement = skinValuesElements[j];
                     let skinValue = await valueElement.getText();
                     if (skinValue.trim() !== "") {
-                        skinsValues.push(parseFloat(skinValue.trim()));
+                        skinValues.push(parseFloat(skinValue.trim()));
                     }
                 }
 
                 // Retrieve chances of item rarities
-                let skinsOdds = [];
+                let skinOdds = [];
                 await driver.wait(
                     until.elementLocated(By.className("odds-number")),
                     5000
@@ -201,51 +199,49 @@ async function handleModalIfExists() {
 
                 // Start the loop from the second element
                 for (let j = 1; j < oddsNumbers.length; j++) {
-                    let oddNumberElement = oddsNumbers[j];
-                    let skinOdd = await oddNumberElement.getText();
+                    let oddsNumberElement = oddsNumbers[j];
+                    let skinOdd = await oddsNumberElement.getText();
                     if (skinOdd.trim() !== "") {
-                        skinsOdds.push(
+                        skinOdds.push(
                             parseFloat(skinOdd.trim().slice(0, -1)) / 100
                         );
                     }
                 }
                 // Add data to the array
-                skin_Odds_Value_tuple.push([skinsValues, skinsOdds]);
-                current_skin++;
+                skinOddsValueTuple.push([skinValues, skinOdds]);
+                currentSkin++;
             }
         }
 
-        console.log("0", skin_Odds_Value_tuple[0]);
-        console.log("1", skin_Odds_Value_tuple[1]);
         // Calculate the global expected value
-        let case_profitability = 0;
-        for (let i = 0; i < skins_global_probas.length; i++) {
-            console.log(`Calculating expected value of skin ${i + 1}...`);
-
-            let item_esperance = 0;
-            for (let j = 0; j < skin_Odds_Value_tuple[i][0].length; j++) {
-                item_esperance +=
-                    skin_Odds_Value_tuple[i][0][j] *
-                    (skin_Odds_Value_tuple[i][1][j] / skins_global_probas[i]);
+        let caseProfitability = 0;
+        for (let i = 0; i < globalSkinProbabilities.length; i++) {
+            console.log(
+                `Calculating expected value of skin ${i + 1}/${caseLength}...`
+            );
+            let itemExpectancy = 0;
+            for (let j = 0; j < skinOddsValueTuple[i][0].length; j++) {
+                itemExpectancy +=
+                    skinOddsValueTuple[i][0][j] *
+                    (skinOddsValueTuple[i][1][j] / globalSkinProbabilities[i]);
             }
-
-            case_profitability += item_esperance * skins_global_probas[i];
+            caseProfitability += itemExpectancy * globalSkinProbabilities[i];
         }
 
         // Display results
         console.log(
-            `\nThe expected return value is ${case_profitability.toFixed(2)} $`
+            `\nThe expected return value is $${caseProfitability.toFixed(2)}`
         );
         console.log(
-            `Minus the case price: ${(case_profitability - case_price).toFixed(
+            `Minus the case price: $${(caseProfitability - casePrice).toFixed(
                 2
-            )} $`
+            )}`
         );
         console.log(
             `Average gain: ${(
                 100 *
-                ((case_profitability - case_price) / case_price)
-            ).toFixed(2)} %`
+                ((caseProfitability - casePrice) / casePrice)
+            ).toFixed(2)}%`
         );
     } catch (error) {
         console.error("An error occurred:", error);
